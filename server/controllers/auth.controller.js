0dde754016f69c5dc3656ed61b54918a5b7b0613
Shounak Kumbhakar <shounakkumbhakar650@gmail.com>
2026-03-28 22:18:51 +0530

    register controller added

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,60 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.models');
+
+// helpers
+const signToken = (user) => {
+    jwt.sign(
+        {id: user._id, email: user.email},
+        process.env.JWT_SECRET,
+        {expiresIn: '7d'}
+    );
+
+    const userPayLoad = (user) => ({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        currency: user.currency
+    });
+}
+
+const registerUser = async (req,res) => {
+    try {
+        const {name, email, password, confirmPassword} = req.body;
+
+        if(!name || !email || !password || !confirmPassword){
+            return res.status(400).json({
+                message: "All the fields are required."
+            });
+        }
+
+        if(password.length() < 6){
+            return res.status(400).json({
+                message: "Password must be of 6 charachters long."
+            });
+        }
+
+        if(password !== confitmPassword){
+            return res.status(400).json({
+                message: "Password didn't match."
+            });
+        }
+
+        const existing = await User.findOne({ email: email.toLowerCase()});
+
+        if(existing){
+            return res.status(400).json({
+                message: "email already registered."
+            });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(password, salt);
+        const user = await User.create({ name, email, passwordHash});
+        const token = signToken(user);
+
+        res.status(201).json({ token, user: userPayLoad(User)});
+    } catch (err) {
+        res.status(500).json({ message: 'Server Error', error: err.message});
+    }
+};
